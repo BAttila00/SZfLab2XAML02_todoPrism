e42a0ae540d8a76507f03623be63b2a4607b6929
Attila Bruder <bruderattila@gmail.com>
2021-12-08 20:04:57 +0100

    API-tól kérjük le a TODO elemeket. És adatkötés rendes megvalósítása.

--- TodoPrism/ViewModels/MainViewModel.cs
+++ TodoPrism/ViewModels/MainViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using Prism.Windows.Mvvm;
+using Prism.Windows.Navigation;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
@@ -6,29 +7,21 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using TodoPrism.Models;
+using TodoPrism.Services;
 
 namespace TodoPrism.ViewModels {
     public class MainViewModel : ViewModelBase {
-        public ObservableCollection<TodoItem> Todos { get; set; } = new ObservableCollection<TodoItem>()
-{
-            new TodoItem()
-            {
-                Id = 1,
-                Title = "Tejet venni",
-                Description = "Ha van tojás, hozz tizet!",
-                Priority = Priority.Normal,
-                IsDone = true,
-                Deadline = DateTimeOffset.Now + TimeSpan.FromDays(1)
-            },
-            new TodoItem()
-            {
-                Id = 2,
-                Title = "Megírni a szakdolgozatot",
-                Description = "Minimum 40 oldal, szépen kitöltve screenshotokkal",
-                Priority = Priority.High,
-                IsDone = false,
-                Deadline = new DateTime(2017, 12, 08, 12, 00, 00, 00)
-            }
-        };
+
+        private ObservableCollection<TodoItem> _todos;
+        public ObservableCollection<TodoItem> Todos {
+            get { return _todos; }
+            set { SetProperty(ref _todos, value); }
+        }
+
+        public override async void OnNavigatedTo(NavigatedToEventArgs e, Dictionary<string, object> viewModelState) {
+            var todos = await new TodoService().GetTodosAsync();
+            Todos = new ObservableCollection<TodoItem>(todos);
+            base.OnNavigatedTo(e, viewModelState);
+        }
     }
 }
